3ae3514dea20f9514b9669fc32a5160d303c4325
lime <lime_bell@naver.com>
2019-07-17 17:58:08 +0900

    Test

--- Libplanet/Net/Swarm.cs
+++ Libplanet/Net/Swarm.cs
@@ -423,7 +423,7 @@ namespace Libplanet.Net
             {
                 _logger.Error(
                     e,
-                    "An unexpected exception occured during StartAsync()");
+                    $"An unexpected exception occured during StartAsync() [{e.ToString()}]");
                 throw;
             }
         }
