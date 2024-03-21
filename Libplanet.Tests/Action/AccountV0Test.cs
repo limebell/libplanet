4641a51ad08d273261588bbe9256f041991ce92e
Chanhyuck Ko <limeelbee@gmail.com>
2024-03-21 17:55:56 +0900

    feat: add dpos related features

--- Libplanet.Tests/Action/AccountV0Test.cs
+++ Libplanet.Tests/Action/AccountV0Test.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Collections.Immutable;
 using Libplanet.Action;
 using Libplanet.Action.State;
 using Libplanet.Action.Tests.Mocks;
 using Libplanet.Crypto;
+using Libplanet.Types.Blocks;
+using Libplanet.Types.Consensus;
 using Xunit.Abstractions;
 
 namespace Libplanet.Tests.Action
@@ -20,12 +24,28 @@ namespace Libplanet.Tests.Action
         {
             IWorld world = new World(new MockWorldState());
             world = world.SetAccount(_accountAddress, delta);
+            var key = new PrivateKey();
+            var lastCommit = new BlockCommit(
+                0,
+                0,
+                default,
+                new[]
+                {
+                    new VoteMetadata(
+                        0,
+                        0,
+                        default,
+                        DateTimeOffset.UtcNow,
+                        key.PublicKey,
+                        VoteFlag.PreCommit).Sign(key),
+                }.ToImmutableArray());
             return new ActionContext(
                 signer,
                 null,
                 signer,
                 0,
                 ProtocolVersion,
+                lastCommit,
                 world,
                 0,
                 0);
